1ab459a975c6256a72a6f0c1f1ba55c8124122d3
Nate Tolbert <ntolbertu85@gmail.com>
2021-04-27 02:18:38 -0400

    04272021 -- Project/Global -- INITIAL COMMIT - Project freshly extracted from Initializr;

---
+++ src/main/java/com/mdspicesmicro/productservice/S09ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mdspicesmicro.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S09ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S09ProductServiceApplication.class, args);
+	}
+
+}
